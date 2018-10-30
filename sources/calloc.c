69eff7cb35e96769af59ff83e0f6c82b58d5b90e
Syberam <sbonnefo@e1r4p17.42.fr>
2018-10-30 18:44:49 +0100

    calloc...

---
+++ sources/calloc.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   calloc.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sbonnefo <sbonnefo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/30 10:27:18 by sbonnefo          #+#    #+#             */
+/*   Updated: 2018/10/30 10:31:16 by sbonnefo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "malloc.h"
+
+void	*calloc(size_t count, size_t size)
+{
+	void	*addr;
+
+	if (!(addr = malloc(count * size)))
+		return NULL;
+	ft_bzero(addr, count * size);
+	return (addr);
+}
