59fc55683177a0d0d0a1b9bd1b2129cd9d81cdb4
sbonnefo persoMac <sbonnefo@student.42.fr>
2018-11-06 08:51:14 +0100

    test gitignore

--- tests/run.sh
+++ tests/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export DYLD_LIBRARY_PATH="/sgoinfre/goinfre/Perso/sbonnefo/Documents/malloc2"
-export DYLD_INSERT_LIBRARIES="/sgoinfre/goinfre/Perso/sbonnefo/Documents/malloc2/libft_malloc.so"
+export DYLD_LIBRARY_PATH="/Users/Administrateur/Documents/42/malloc2"
+export DYLD_INSERT_LIBRARIES="/Users/Administrateur/Documents/42/malloc2/libft_malloc.so"
 export DYLD_FORCE_FLAT_NAMESPACE=1
 $@
